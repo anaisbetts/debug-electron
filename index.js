7af59aafc4185ff9d70635e33378d24de9e97aa5
Paul Betts <paul@paulbetts.org>
2016-11-09 16:22:51 -0800

    Debug is fixed directly!

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-if (process.type !== 'renderer') {
-  module.exports = require('debug');
-} else {
-  module.exports = require('debug/browser');
-  if (process && process.env && process.env.DEBUG) module.exports.enable(process.env.DEBUG);
-}
+module.exports = require('debug');
